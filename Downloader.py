85fb965d7c4bb32058560cd0a26be410b50390c2
campana-giuseppe <68437003+campana-giuseppe@users.noreply.github.com>
2020-12-15 20:04:54 +0100

    Update Downloader.py

--- Downloader.py
+++ Downloader.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import urllib.request
-
+#downloader of images from a list of urls on a cvs file
 
 def url_to_jpg(i, url, file_path):
 
-    filename = '50{}.jpg'.format(i)
+    filename = '{}.jpg'.format(i)
     full_path = '{}{}'.format(file_path, filename)
     urllib.request.urlretrieve(url, full_path)
 
@@ -13,8 +13,8 @@ def url_to_jpg(i, url, file_path):
     return None
 
 
-FILENAME = 'lista_down.csv'
-FILE_PATH = 'download/'
+FILENAME = 'lista_down.csv' #file name csv with links
+FILE_PATH = 'download/'   #folder name to save images (placed in the same folder of py file)
 
 urls = pd.read_csv(FILENAME)
 
